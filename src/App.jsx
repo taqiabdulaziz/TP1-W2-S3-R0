8e0bd76cc3d47afbda3956fa38aa14995cce0e05
Taqi Aziz <taqi.aziz@tech.jago.com>
2022-12-03 22:12:20 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
