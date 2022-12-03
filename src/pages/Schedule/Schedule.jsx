51cbeaec5293cbb76d753a51413a8e300c9de0b3
Taqi Aziz <taqi.aziz@tech.jago.com>
2022-12-04 04:43:15 +0700

    add background iamge

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -5,28 +5,30 @@ import ScheduleConstant from './ScheduleConstant';
 const Schedule = () => {
   return (
     <div className="container">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Mata Kuliah</th>
-            <th scope="col">Tanggal</th>
-            <th scope="col">Jam</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            ScheduleConstant.schedules.map((schedule, i) => (
-              <tr>
-                <th scope="row">{i + 1}</th>
-                <td>{schedule.subject}</td>
-                <td>{schedule.date}</td>
-                <td>{schedule.time}</td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div className='table-wrapper'>
+        <table class="table" className='table'>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Mata Kuliah</th>
+              <th scope="col">Tanggal</th>
+              <th scope="col">Jam</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              ScheduleConstant.schedules.map((schedule, i) => (
+                <tr>
+                  <th scope="row">{i + 1}</th>
+                  <td>{schedule.subject}</td>
+                  <td>{schedule.date}</td>
+                  <td>{schedule.time}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
